6b64b336aa0281e93d2aeb3cdf1fc1651af6bb0a
Jacques Ehlers <jacques.ehlers@nintex.com>
2024-07-05 10:43:23 +0200

    asdf

--- playwright-secret/tests/secret-test.spec.js
+++ playwright-secret/tests/secret-test.spec.js
@@ -2,13 +2,13 @@
 const { test, expect } = require('@playwright/test');
 
 test('Smoke 2 - has title', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com//');
+  await page.goto('https://www.saucedemo.com/');
 
   await expect(page).toHaveTitle(/Swag Labs/);
 
-  await page.fill('input#user-name', 'whatever@what.com');
+  await page.fill('input#user-name', 'standard_user');
 
   const value = await page.inputValue('input#user-name');
-  expect(value).toBe('whatever@what.com');
+  expect(value).toBe('standard_user');
 
 });
