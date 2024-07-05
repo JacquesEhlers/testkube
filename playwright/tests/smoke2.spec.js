76212471feef83d5f2454e2160b65fd82d9146ef
Jacques Ehlers <jacques.ehlers@nintex.com>
2024-07-05 08:39:43 +0200

    test test

--- playwright/tests/smoke2.spec.js
+++ playwright/tests/smoke2.spec.js
@@ -2,7 +2,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Smoke 2 - has title', async ({ page }) => {
-  await page.goto('https://ndp-dev.platform.nintextest.io/');
+  await page.goto('https://ndp.platform.nintextest.io/');
 
   await expect(page).toHaveTitle(/NDP - Nintex Developer Portal/);
 });
