e2d571e731bca8c0f90b26376c3c310b65a2eee1
Jacques Ehlers <jacques.ehlers@nintex.com>
2024-07-08 07:47:03 +0200

    quote fix

--- playwright/tests/smoke2.spec.js
+++ playwright/tests/smoke2.spec.js
@@ -2,13 +2,12 @@
 const { test, expect } = require('@playwright/test');
 
 test('Smoke 2 - has title', async ({ page }) => {
-  await page.goto('https://testing-playground.com//');
+  await page.goto('https://testing-playground.com/');
 
   await expect(page).toHaveTitle(/Testing Playground/);
 
-  await page.fill('input#email"]', 'whatever@what.com');
+  await page.fill('input#email', 'whatever@what.com');
 
   const value = await page.inputValue('input#email');
   expect(value).toBe('whatever@what.com');
-
 });
